d7a49d94f12553039c5a9b323a22dda02e491188
joymon <joymon@gmail.com>
2017-08-29 15:45:29 -0400

    End to end operation correlation - From Angular app to intermediate web service to internal net.pipe service.

--- application-insights/correlation/AngularClient/JS/Controllers.js
+++ application-insights/correlation/AngularClient/JS/Controllers.js
@@ -3,23 +3,38 @@
 /// <reference path="Services.js" />
 
 app.controller("RESTClientController", ['$scope', '$log', 'RESTClientService', function ($scope, $log, RESTClientService) {
-    //debugger;
     function createGuid() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     }
+    $scope.findArea = function () {
+        $scope.operation_id = createGuid();
+        window.appInsights.context.operation.id = $scope.operation_id;
+        window.appInsights.trackEvent("Custom event from JS before service call", { operation_Id: $scope.operation_id });
 
-    $scope.operation_id = createGuid();
-    window.appInsights.trackEvent("Custom event from JS", { operation_Id: $scope.operation_id });
+        var promiseGet = RESTClientService.get($scope.radius, $scope.operation_id);
+        promiseGet.then(function (pl) {
+            $scope.message = pl.data;
+        },
+                  function (errorPl) {
+                      $log.error('failure loading Company', errorPl);
+                  });
+    }
 
-    var promiseGet = RESTClientService.get($scope.operation_id);
-    promiseGet.then(function (pl) {
-        $scope.message = pl.data;
-    },
-              function (errorPl) {
-                  $log.error('failure loading Company', errorPl);
-              });
+    $scope.firstAI = function () {
+        $scope.operation_id = createGuid();
+        window.appInsights.queue.push(function () {
+            window.appInsights.context.addTelemetryInitializer(function (envelope) {
+                envelope.tags["ai.operation.id"] = $scope.operation_id;
+                envelope.tags["ai.operation.name"] = "my-client-custom-init-op";
+                console.log("'ai.operation.id' init " + $scope.operation_id);
+            });
+        });
+        window.appInsights.trackEvent("Custom init event from JS", { operation_Id: $scope.operation_id });
+
+    }
+    $scope.firstAI();
 }]);
 
--- application-insights/correlation/AngularClient/JS/Services.js
+++ application-insights/correlation/AngularClient/JS/Services.js
@@ -1,34 +1,15 @@
-﻿/// <reference path="../angular.min.js" />
-/// <reference path="../angular-resource.min.js" />
-/// <reference path="Modules.js" />
-
-app.service("RESTClientService", function ($http) {
-    //http://localhost/FrontEndWCFService/FrontEndService.svc/GetAreaOfCircle?radius=3
-    var url = "http://localhost/FrontEndWCFService/FrontEndService.svc/areaOf/3";
-    this.get = function (operation_id) {
+﻿app.service("RESTClientService", function ($http) {
+        
+    this.get = function (radius, operation_id) {
+        var url = "http://localhost/FrontEndWCFService/FrontEndService.svc/areaOf/" + radius;
         var guid = operation_id;
-        //How to pass this operation id from here to server so that server can use it as parent id????
-        var cc = 'operationId=' + guid + ',operation_Id=' + guid + ',"x-ms-request-id"='+guid;
+        //The RequestId header passed from here is taken as operation id at webhttp WCF side to correlate.
         return $http.get(url, {
             headers: {
-                "operation_id": guid,
-                "operationId": guid,
-                operation_Id: guid,
                 'Request-Id':guid,
-                'x-ms-request-id':guid,
-                "Correlation-Context" : cc,
-                "operation_name": "Request from Angular"
+                //'x-ms-request-id':guid,
             }
         }
         );
     };
-
-    function createGuid() {
-        function S4() {
-            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        }
-        return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
-    }
-
-
 });
